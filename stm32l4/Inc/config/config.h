9d05b4507e18dd1279fccca4e92d6e14b6356f4c
Rainer Both <bimser50@hotmail.com>
2026-02-26 21:13:47 +0100

    Update config.h

--- stm32l4/Inc/config/config.h
+++ stm32l4/Inc/config/config.h
@@ -37,12 +37,12 @@
  ********************************************************************************
  */ 
 //#define NOEXTENSION 
-//#define UNIVERSAL 
+#define UNIVERSAL 
 //#define MULTITEMP
 //#define ENVIRONMENTAL
 //#define TX18LISTENER
 //#define PWM_DEVICE        
-#define IO_DEVICE  
+//#define IO_DEVICE  
 
 /*
  ********************************************************************************
@@ -72,7 +72,7 @@
 #define USE_SECONDTIMER     1
 #define USE_HW_PWMTIMER     1
 #define USE_BASICTIMER      1
-#define USE_QSPI            0            // When QSPI flash is installed, always USE it, otherwise it will consume roundabout 2mA in uninitialized state!
+#define USE_QSPI            1            // When QSPI flash is installed, always USE it, otherwise it will consume roundabout 2mA in uninitialized state!
 #define USE_OSPI1           0            // When OSPI flash is installed, always USE it, otherwise it will consume roundabout 2mA in uninitialized state!
 #define USE_OSPI2           0            
 #define USE_CAN             0
